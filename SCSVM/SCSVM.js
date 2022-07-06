25cee23dd51a7f92512a4786a3c6e4ea96ff84c5
wWrongName <name.johnny@mail.ru>
2022-07-06 12:19:20 +0300

    feat: add logger

--- SCSVM/SCSVM.js
+++ SCSVM/SCSVM.js
@@ -1,54 +1,7 @@
-const path = require("path")
-
-const argsSet = require(path.join(__dirname, "./utils/args.json"))
-const yargsConfigurator = require(path.join(__dirname, "./utils/yargsConfigurator"))
 
 
 class SCSVM {
-    constructor (config) {
-        yargsConfigurator.init(argsSet)
-        const argv = yargsConfigurator.argv()
 
-        let getKeys = () => {
-            return Object.keys(argv).reduce((prev, cur, index) => {
-                if (cur !== "_" && cur.indexOf("$") === -1)
-                    prev.push(cur)
-                return prev
-            }, [])
-        }
-
-        let setNewParam = (configPath, newVal) => {
-            let path = configPath.split(".")
-            let tmpPointer = this.config
-            path.forEach((prop, index) => {
-                if (path.length - 1 === index)
-                    tmpPointer[prop] = newVal
-                else
-                    tmpPointer = tmpPointer[prop]
-            })
-        }
-
-        let getArgObj = (searchedValue) => {
-            let args = Object.keys(argsSet)
-            let foundArg = args.find(arg => arg === searchedValue)
-            if (foundArg !== undefined)
-                return argsSet[foundArg]
-
-            for (let i = 0; i < args.length; i++) {
-                if (argsSet[args[i]].alias === searchedValue)
-                    return argsSet[args[i]]
-            }
-        }
-
-        this.config = config
-        getKeys().forEach(argName => {
-            let argObj = getArgObj(argName)
-            if (argObj !== undefined && argObj.type === typeof argv[argName])
-                setNewParam(argObj.configPath, argv[argName])
-        })
-
-        console.log(this.config)
-    }
 }
 
 module.exports = SCSVM
--- index.js
+++ index.js
@@ -1,6 +1,77 @@
+const winston = require('winston')
 const path = require("path")
+
+const yargsConfigurator = require(path.join(__dirname, "./utils/yargsConfigurator"))
+const argsSet = require(path.join(__dirname, "./utils/args.json"))
 const config = require(path.join(__dirname, "config.js"))
 
 const SCSVM = require("./SCSVM/SCSVM")
 
-const scsvm = new SCSVM(config)
+
+class Interface {
+    constructor (config)  {
+        let initLogger = () => {
+            this.logger = winston.createLogger({
+                level: this.config.log.level,
+                format: winston.format.combine(
+                    winston.format.timestamp({format: 'YYYY-MM-DD HH:mm:ss'}),
+                    winston.format.printf(printfInfo => {
+                        return `[${printfInfo.level.toUpperCase()}][${printfInfo.timestamp}] - ${printfInfo.message}`
+                    })
+                ),
+                transports: [
+                    new winston.transports.File({ filename: this.config.log.path }),
+                    new winston.transports.Console
+                ]
+            })
+        }
+
+        let getKeys = () => {
+            return Object.keys(argv).reduce((prev, cur, index) => {
+                if (cur !== "_" && cur.indexOf("$") === -1)
+                    prev.push(cur)
+                return prev
+            }, [])
+        }
+
+        let setNewParam = (configPath, newVal) => {
+            let path = configPath.split(".")
+            let tmpPointer = this.config
+            path.forEach((prop, index) => {
+                if (path.length - 1 === index)
+                    tmpPointer[prop] = newVal
+                else
+                    tmpPointer = tmpPointer[prop]
+            })
+        }
+
+        let getArgObj = (searchedValue) => {
+            let args = Object.keys(argsSet)
+            let foundArg = args.find(arg => arg === searchedValue)
+            if (foundArg !== undefined)
+                return argsSet[foundArg]
+
+            for (let i = 0; i < args.length; i++) {
+                if (argsSet[args[i]].alias === searchedValue)
+                    return argsSet[args[i]]
+            }
+        }
+
+        let mergeConfig = () => {
+            this.config = config
+            getKeys().forEach(argName => {
+                let argObj = getArgObj(argName)
+                if (argObj !== undefined && argObj.type === typeof argv[argName])
+                    setNewParam(argObj.configPath, argv[argName])
+            })
+        }
+
+        const argv = yargsConfigurator.init(argsSet).argv()
+        mergeConfig()
+        initLogger()
+
+        this.scsvm = new SCSVM()
+    }
+}
+
+const scsvmInterface = new Interface(config)
